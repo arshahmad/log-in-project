75ca7d4096bf0607b7cc9b55cb22b8243cf56d8e
Arsh Ahmad <arshahmadhdi@gmail.com>
2019-01-13 15:20:30 +0530

    m1

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  name = 'anony';
+  constructor(private user:UserService) { }
+
+  ngOnInit() {
+    this.name = this.user.username;
+    console.log('is user logged in?', this.user.getUserLoggedIn())
+
+  }
+
+}
